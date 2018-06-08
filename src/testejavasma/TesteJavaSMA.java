15315c29f35a1780aca1de054c30dbdd01f2351c
laboratorio <laboratorio@LaboratorioA12.labifms.edu.br>
2018-06-07 21:22:17 -0400

    Este é apenas um código de teste de uso do Git Primeito commit

---
+++ src/testejavasma/TesteJavaSMA.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testejavasma;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class TesteJavaSMA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Ola mundo");
+        System.out.println("Semana do Meio Ambiente");
+        // TODO code application logic here
+    }
+    
+}
